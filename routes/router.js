6172565fb8f0b9047f6e4f08141a0fa0f6adb9eb
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-01-02 21:06:45 +0530

    secure set to true again

--- routes/router.js
+++ routes/router.js
@@ -99,7 +99,7 @@ router.post("/owner-login", async (req, res) => {
       res.cookie("jwtoken", token, {
         expires: new Date(Date.now() + 36000000),
         httpOnly: true,
-        secure: false, // Set to true in production (for HTTPS)
+        secure: true, // Set to true in production (for HTTPS)
         sameSite: "none"
       });
 
